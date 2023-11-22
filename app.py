045486e36c16173b9926f3bf662f52d7c2ae3d23
aaroln <lahtinenaaro77@gmail.com>
2023-11-22 23:14:31 +1100

    changes to port

--- app.py
+++ app.py
@@ -68,7 +68,5 @@ def plot_svg(num_x_points=50):
 
 
 if __name__ == "__main__":
-    import webbrowser
-
-    webbrowser.open("http://127.0.0.1:5000/")
-    app.run(debug=True)
+    
+    app.run(debug=True, port=8080)
