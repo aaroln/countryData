2e990137e111c9ff5f7a3247e9cbbb459daec86d
aaroln <lahtinenaaro77@gmail.com>
2023-11-23 21:25:32 +1100

    box working

--- application/app.py
+++ application/app.py
@@ -12,15 +12,26 @@ dfLandCover = pd.read_csv('landCover.csv')
 dfUrbanLand = pd.read_csv('urbanLand.csv')
 dfUrbanPop = pd.read_csv('urbanPop.csv')
 
+# Getting country list and capitalising it
+listCountry = dfPop['Country Name'].tolist()
+listCountry = [s.upper() for s in listCountry]
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     user_input = None
+    invalid_entry = False
     
     if request.method == 'POST':
-        user_input = request.form['user_input']
-    
-    return render_template('index.html', user_input=user_input)
+        user_input = request.form['user_input'].upper()
+        if user_input not in listCountry:
+            invalid_entry = True
+
+    return render_template('index.html', user_input=user_input, invalid_entry=invalid_entry)
+
+
+@app.route('/')
+def popChart():
 
 if __name__ == "__main__":
     app.run(debug=True)
